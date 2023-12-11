a462ace44f07e283160ba6712077ce3ee801e2f2
gianmarcocilia <gianmarcocilia97@gmail.com>
2023-12-11 15:09:39 +0100

    add: text length

--- greeting.php
+++ greeting.php
@@ -3,6 +3,7 @@ $message = $_GET['message'];
 $word = $_GET['word'];
 
 $new_message = str_replace($word, '***', $message);
+$message_len = strlen($message);
 ?>
 
 <!DOCTYPE html>
@@ -15,5 +16,6 @@ $new_message = str_replace($word, '***', $message);
 <body>
     <h2>Ecco il nuovo messaggio! La parola censurata è: <?php echo $word ?></h2>
     <p><?php echo $new_message ?></p>
+    <h5>La lunghezza del testo è di: <?php echo $message_len ?></h5>
 </body>
 </html>
